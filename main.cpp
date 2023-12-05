f74cdc79406709e8cb904a4d048ea62d84a94d4a
moraeszete <lucas.moraes24536@gmail.com>
2023-12-05 13:48:50 -0300

    ajustes finais apresnetação

--- main.cpp
+++ main.cpp
@@ -3,59 +3,60 @@
 #include <string.h>
 #include <locale.h>
 
-#define MAX_PRODUTOS 100
+#define MAX_PRODUTOS 10
 #define MAX_NOME 50
 
-
-
-// Definição da struct para o produto
-struct Produto {
+// Struct do produto (objeto)
+struct Produto
+{
     int codigo;
     char nome[MAX_NOME];
     int quantidade;
     float preco;
 };
 
-// Função para carregar o banco de dados a partir de um arquivo
-void carregarBanco(struct Produto estoque[], int *numProdutos) {
-    FILE *arquivo = fopen("estoque.txt", "r");
-    if (arquivo == NULL) {
+// Função para salvar o banco de dados em um arquivo
+void salvarBanco(const struct Produto estoque[], int *numProdutos)
+{
+    FILE *arquivo = fopen("estoque.csv", "w"); 
+    if (arquivo == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    for (int i = 0; i < *numProdutos; i++)
+    {
+        fprintf(arquivo, "%d %s %d %.2f\n", estoque[i].codigo, estoque[i].nome, estoque[i].quantidade, estoque[i].preco);
+    }
+
+    fclose(arquivo);
+}
+
+// Botão 1: Carregar/subir arquivo do Banco
+void carregarBanco(struct Produto estoque[], int *numProdutos)
+{
+    FILE *arquivo = fopen("estoque.csv", "r"); 
+    if (arquivo == NULL)
+    {
         printf("Erro ao abrir o arquivo.\n");
         return;
     }
 
     *numProdutos = 0;
-    while (fscanf(arquivo, "%d %s %d %f", &estoque[*numProdutos].codigo, estoque[*numProdutos].nome,
-           &estoque[*numProdutos].quantidade, &estoque[*numProdutos].preco) == 4) {
+    while (*numProdutos < MAX_PRODUTOS && fscanf(arquivo, "%d %s %d %f", &estoque[*numProdutos].codigo, estoque[*numProdutos].nome, &estoque[*numProdutos].quantidade, &estoque[*numProdutos].preco) == 4)
+    {
         (*numProdutos)++;
-        if (*numProdutos >= MAX_PRODUTOS) {
-            printf("Limite de produtos atingido.\n");
-            break;
-        }
     }
 
     fclose(arquivo);
 }
 
-// Função para salvar o banco de dados em um arquivo
-void salvarBanco(const struct Produto estoque[], int numProdutos) {
-    FILE *arquivo = fopen("estoque.txt", "w");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return;
-    }
-
-    for (int i = 0; i < numProdutos; i++) {
-        fprintf(arquivo, "%d %s %d %.2f\n", estoque[i].codigo, estoque[i].nome,
-                estoque[i].quantidade, estoque[i].preco);
-    }
-
-    fclose(arquivo);
-}
-
-// Função para cadastrar um novo produto
-void cadastrarProduto(struct Produto estoque[], int *numProdutos) {
-    if (*numProdutos >= MAX_PRODUTOS) {
+// Botão 2: Cadastrar novo produto
+void cadastrarProduto(struct Produto estoque[], int *numProdutos)
+{
+    if (*numProdutos >= MAX_PRODUTOS)
+    {
         printf("Limite de produtos atingido.\n");
         return;
     }
@@ -64,7 +65,7 @@ void cadastrarProduto(struct Produto estoque[], int *numProdutos) {
     scanf("%d", &estoque[*numProdutos].codigo);
 
     printf("Digite o nome do produto: ");
-    scanf("%s", estoque[*numProdutos].nome);
+    scanf("%s", &estoque[*numProdutos].nome);
 
     printf("Digite a quantidade disponível: ");
     scanf("%d", &estoque[*numProdutos].quantidade);
@@ -73,54 +74,129 @@ void cadastrarProduto(struct Produto estoque[], int *numProdutos) {
     scanf("%f", &estoque[*numProdutos].preco);
 
     (*numProdutos)++;
+    system("cls");
 }
 
-// Função para listar todos os produtos
-void listarProdutos(const struct Produto estoque[], int numProdutos) {
+// Botão 3: listar todos os produtos
+void listarProdutos(const struct Produto estoque[], int *numProdutos)
+{
     printf("Lista de Produtos:\n");
-    for (int i = 0; i < numProdutos; i++) {
-        printf("Código: %d, Nome: %s, Quantidade: %d, Preço: %.2f\n",
-               estoque[i].codigo, estoque[i].nome, estoque[i].quantidade, estoque[i].preco);
+    for (int i = 0; i < *numProdutos; i++)
+    {
+        printf("Código: %d, Nome: %s, Quantidade: %d, Preço: %.2f\n", estoque[i].codigo, estoque[i].nome, estoque[i].quantidade, estoque[i].preco);
     }
 }
 
-int main() {
-	setlocale (LC_ALL,"");
-	
+// Botão 4: Excluir produto
+void excluirProduto(struct Produto estoque[], int *numProdutos)
+{
+    int troca;
+
+    printf("Digite o código do produto a ser excluído: ");
+    scanf("%d", &troca);
+
+    for (int i = 0; i < *numProdutos; i++)
+    {
+        if (estoque[i].codigo == troca)
+        {
+            for (int j = i; j < *numProdutos - 1; j++)
+            {
+                estoque[j] = estoque[j + 1];
+            }
+
+            (*numProdutos)--;
+            printf("Produto excluído com sucesso.\n");
+            return;
+            system("cls");
+        }
+    }
+    printf("Produto não encontrado.\n");
+    system("cls");
+}
+// Botão 5: Alterar produto
+void alterarProduto(struct Produto estoque[], int *numProdutos)
+{
+    int troca;
+
+    printf("Digite o código do produto a ser alterado: ");
+    scanf("%d", &troca);
+
+    for (int i = 0; i < *numProdutos; i++)
+    {
+        if (estoque[i].codigo == troca)
+        {
+            printf("Digite o novo código do produto: ");
+            scanf("%d", &estoque[i].codigo);
+
+            printf("Digite o novo nome do produto: ");
+            scanf("%s", &estoque[i].nome);
+
+            printf("Digite a nova quantidade disponível: ");
+            scanf("%d", &estoque[i].quantidade);
+
+            printf("Digite o novo preço de venda: ");
+            scanf("%f", &estoque[i].preco);
+
+            printf("Produto alterado com sucesso.\n");
+            system ("cls");
+            return;
+        }
+    }
+
+    printf("Produto não encontrado.\n");
+}
+int main()
+{
+    setlocale(LC_ALL, "");
+
     struct Produto estoque[MAX_PRODUTOS];
     int numProdutos = 0;
     int opcao;
 
-    do {
+    do
+    {
         printf("\nMenu:\n");
         printf("1 - Carregar Banco de Dados\n");
         printf("2 - Cadastrar produto\n");
         printf("3 - Listar produtos\n");
+        printf("4 - Excluir produto\n");
+        printf("5 - Alterar produto\n");
         printf("0 - Sair\n");
 
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
 
-        switch (opcao) {
-            case 1:
-                carregarBanco(estoque, &numProdutos);
-                break;
+        switch (opcao)
+        {
+        case 1:
+            carregarBanco(estoque, &numProdutos);
+            system("cls");
+            break;
 
-            case 2:
-                cadastrarProduto(estoque, &numProdutos);
-                salvarBanco(estoque, numProdutos);
-                break;
+        case 2:
+            cadastrarProduto(estoque, &numProdutos);
+            salvarBanco(estoque, &numProdutos);
+            system("cls");
+            break;
 
-            case 3:
-                listarProdutos(estoque, numProdutos);
-                break;
+        case 3:
+            listarProdutos(estoque, &numProdutos);
+            break;
 
-            case 0:
-                printf("Saindo do programa.\n");
-                break;
+        case 4:
+            excluirProduto(estoque, &numProdutos);
+            break;
 
-            default:
-                printf("Opção inválida. Tente novamente.\n");
+        case 5:
+            alterarProduto(estoque, &numProdutos);
+            break;
+
+        case 0:
+            printf("Saindo do programa.\n");
+            break;
+
+        default:
+            printf("Opção inválida. Tente novamente.\n");
         }
 
     } while (opcao != 0);
